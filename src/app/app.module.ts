d0a2efe18ed36d7c187722ec8094dffc4a337f85
gozek <g_0zek@hotmail.com>
2016-09-08 22:10:35 +0300

    updated index file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,13 @@ import { routing } from './app.routes';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
-import { DashboardHeaderComponent } from './dashboard/dashboard-header/dashboard-header.component';
-import { GraphHistogramComponent } from './dashboard/graph-histogram/graph-histogram.component';
-import { GraphSimilaritiesComponent } from './dashboard/graph-similarities/graph-similarities.component';
-import { KnownTargetsComponent } from './dashboard/known-targets/known-targets.component';
-import { TopCompoundsComponent } from './dashboard/top-compounds/top-compounds.component';
-import { SettingsHeaderComponent } from './settings/settings-header/settings-header.component';
-import { InputFormComponent } from './settings/input-form/input-form.component';
+import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
+import { GraphHistogramComponent } from './graph-histogram/graph-histogram.component';
+import { GraphSimilaritiesComponent } from './graph-similarities/graph-similarities.component';
+import { KnownTargetsComponent } from './known-targets/known-targets.component';
+import { TopCompoundsComponent } from './top-compounds/top-compounds.component';
+import { SettingsHeaderComponent } from './settings-header/settings-header.component';
+import { InputFormComponent } from './input-form/input-form.component';
 import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
 
 @NgModule({
@@ -28,7 +28,7 @@ import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
     TopCompoundsComponent,
     SettingsHeaderComponent,
     InputFormComponent,
-    SidenavbarComponent
+    SidenavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,7 +37,7 @@ import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
     routing
   ],
   providers: [
-      
+
   ],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]

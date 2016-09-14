6d080fe82bb1525c0c25f4fbeaf85ad16e3196d3
herkulano <im@herkulano.com>
2016-09-14 11:07:27 +0300

    fixed testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,24 +3,46 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: TestLuciusWebApp', () => {
+import { MdCoreModule } from '@angular2-material/core';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdIconModule } from '@angular2-material/icon';
+
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './reducers';
+import { actions } from './actions';
+
+describe('App: LuciusWeb', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        MdCoreModule,
+        MdButtonModule,
+        MdSidenavModule,
+        MdIconModule,
+
+        StoreModule.provideStore(
+          reducers
+        )
+      ],
+      providers: [
+        actions
       ],
     });
   });
 
   it('should create the app', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    let element = fixture.debugElement.componentInstance;
+    expect(element).toBeTruthy();
   }));
 
   it(`should have a sidenav`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.nativeElement;
-    expect(app.querySelector(':host(md-sidenav-layout > md-sidenav)')).toBeTruthy();
+    let element = fixture.debugElement.nativeElement;
+    expect(element.querySelector('md-sidenav-layout > md-sidenav')).toBeTruthy();
   }));
 });
--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -3,12 +3,41 @@
 import { TestBed, async } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
 
-import { AppState } from '../../reducers';
-import { NavActions } from '../../actions/nav';
+import { MdCoreModule } from '@angular2-material/core';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdIconModule } from '@angular2-material/icon';
+
+import { StoreModule } from '@ngrx/store';
+import { reducers } from '../../reducers';
+import { actions } from '../../actions';
 
 describe('Component: Dashboard', () => {
-  it('should create an instance', () => {
-    let component = new DashboardComponent(null, null);
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DashboardComponent
+      ],
+      imports: [
+        MdCoreModule,
+        MdButtonModule,
+        MdIconModule,
+        MdToolbarModule,
+
+        StoreModule.provideStore(
+          reducers
+        )
+      ],
+      providers: [
+        actions
+      ],
+    });
   });
+
+  it('should create an instance', async(() => {
+    let fixture = TestBed.createComponent(DashboardComponent);
+    let element = fixture.debugElement.componentInstance;
+    expect(element).toBeTruthy();
+  }));
+
 });

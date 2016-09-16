c91be211c10e7f92f7aed128646c72ed2d85f188
gozek <g_0zek@hotmail.com>
2016-09-16 17:34:10 +0300

    changed router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/let';
 import { AppState, getSidenavOpened, checkIfDashEnabled } from './reducers';
 import { NavActions } from './actions/nav';
 import { DashActions } from './actions/dash';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,36 +16,47 @@ import { Router } from '@angular/router';
 export class AppComponent {
   sidenavOpen$: Observable<Boolean>;
   dashEnabled$: Observable<Boolean>;
-  currentComponent: string;
+  activePage$: Observable<String>;
+  currentUrl: String;
 
   constructor(
     private store: Store<AppState>,
     private navActions: NavActions,
     private dashActions: DashActions,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
-    this.currentComponent = 'Dashboard';
+    // local variable for currentUrl
+    this.currentUrl = '/';
+    this.router.events.subscribe(ev => this.currentUrl = ev.url);
+    console.log(this.currentUrl);
+
+    // observable to use with async pipe - not working!
+    this.activePage$ = route.url.map(x => x.join(''));
+
     this.sidenavOpen$ = store
       .let(getSidenavOpened());
     this.dashEnabled$ = store
       .let(checkIfDashEnabled());
   }
 
-  navToDasboard() {
-      // call to the store to update activeComponent
-      this.currentComponent = 'Dashboard';
-      this.store.dispatch(this.dashActions.activateDashboard(true));
-      this.router.navigate(['/dashboard']);
-      this.closeSidenav();
-  }
+  // navToDasboard() {
+  //     // call to the store to update activeComponent
+  //     this.currentComponent = 'Dashboard';
+  //     this.store.dispatch(this.dashActions.activateDashboard(true));
+  //     this.router.navigate(['/dashboard']);
+  //     this.closeSidenav();
+  // }
+  //
+  // navToSettings() {
+  //     // call to the store to update activeComponent
+  //     this.currentComponent = 'Settings';
+  //     this.store.dispatch(this.dashActions.activateDashboard(false));
+  //     this.router.navigate(['/settings']);
+  //     this.closeSidenav();
+  // }
 
-  navToSettings() {
-      // call to the store to update activeComponent
-      this.currentComponent = 'Settings';
-      this.store.dispatch(this.dashActions.activateDashboard(false));
-      this.router.navigate(['/settings']);
-      this.closeSidenav();
-  }
+
 
   openSidenav() {
     this.store.dispatch(this.navActions.toggleSidenav(true));

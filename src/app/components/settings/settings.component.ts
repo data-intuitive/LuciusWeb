565d42bd6ec40da9d0f0309891bfe6615ad71fed
gozek <g_0zek@hotmail.com>
2016-09-21 17:19:11 +0300

    refactored settings component

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent {
 
-  constructor() {
-  }
+export class SettingsComponent implements OnInit {
+    settingsForm: FormGroup;
+
+    constructor(private _formBuilder: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.settingsForm = this._formBuilder.group({
+            plotNoise: Number,
+            hist2dBins: Number,
+            hist2dNoise: Number,
+            histogramBins: Number,
+            topComps: Number,
+            serverURL: ['', Validators.required],
+            queryStr: ['', Validators.required],
+            classPath: ['', Validators.required],
+            sourireURL: ['', Validators.required],
+            hiddenComps: Boolean
+        });
+    }
+
+    onSubmit() {
+            console.log(this.settingsForm.value);
+    }
 }

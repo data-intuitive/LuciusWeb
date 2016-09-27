a8226ffc50ab5484a14ee8a43775ff43108bf3b2
Herculano Campos <im@herkulano.com>
2016-09-28 02:00:33 +0300

    test(e2e): fix e2e test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/let';

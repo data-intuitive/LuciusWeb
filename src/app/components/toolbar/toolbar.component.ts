4b1bea19e6b424132d434ebb85c79b0ec618dfce
gozek <g_0zek@hotmail.com>
2016-10-31 00:32:55 +0200

    update toolbar and styles

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import * as fromRoot from '../../reducers';
@@ -10,8 +10,9 @@ import * as layoutActions from '../../actions/layout';
   styleUrls: ['./toolbar.component.scss']
 })
 
-export class ToolbarComponent {
-  @Input() type: String = '';
+export class ToolbarComponent implements OnInit {
+  @Input() type: string = '';
+  showMe: boolean;
 
   constructor(
     private store: Store<fromRoot.State>
@@ -21,6 +22,15 @@ export class ToolbarComponent {
   // open side navigation bar by updating store, when menu button is pressed
   openSidenav() {
     this.store.dispatch(new layoutActions.OpenSidenavAction());
+  }
+
+  ngOnInit() {
+    console.log(this.type);
+    if (this.type === 'settings') {
+      this.showMe = false;
+    }else {
+      this.showMe = true;
+    }
   }
 
 }

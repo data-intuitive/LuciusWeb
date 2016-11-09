9c2b0505ee389982c7b51c5328b69b6cd2b25786
gozek <g_0zek@hotmail.com>
2016-11-09 15:14:34 +0200

    update .travis.yml

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { LocalStorageService } from './localstorage.service';
+import { LocalStorageService } from './local-storage.service';
 
 describe('Service: GetData', () => {
   beforeEach(() => {

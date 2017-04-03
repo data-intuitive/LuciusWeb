b357a036260bfab78817ad73b49bd37b3b83cb55
Toni Verbeiren <toni.verbeiren@gmail.com>
2017-04-03 12:50:49 +0200

    Status indicator when loading

--- src/js/components/Table.js
+++ src/js/components/Table.js
@@ -1,7 +1,8 @@
 import xs from 'xstream';
 import sampleCombine from 'xstream/extra/sampleCombine';
 import debounce from 'xstream/extra/debounce'
-import { a, h, p, div, br, label, input, code, table, tr, td, b, h2, button, svg, h5, th, thead, tbody, i, span } from '@cycle/dom';
+import delay from 'xstream/extra/delay'
+import { a, h, p, div, br, label, input, code, table, tr, td, b, h2, button, svg, h5, th, thead, tbody, i, span, ul, li } from '@cycle/dom';
 import { clone } from 'ramda';
 import { log } from '../utils/logger'
 import { ENTER_KEYCODE } from '../utils/keycodes.js'
@@ -10,7 +11,7 @@ import { SampleTable } from './SampleTable/SampleTable'
 import isolate from '@cycle/isolate'
 import { merge } from 'ramda'
 import dropRepeats from 'xstream/extra/dropRepeats'
-// import { clone } from 'ramda';
+import dropUntil from 'xstream/extra/dropUntil'
 
 export function Table(sources) {
 
@@ -64,7 +65,8 @@ export function Table(sources) {
 	// Extract the data from the result
 	// TODO: check for errors coming back
 	const resultData$ = response$.map(response => response.body.result.data);
-    const data$ = resultData$;
+    const data$ = resultData$.debug('data here! -----------------')
+                    // .startWith([])
 
     // Delegate effective rendering to SampleTable:
     const sampleTable = isolate(SampleTable, 'result')(sources);
@@ -96,9 +98,9 @@ export function Table(sources) {
                 console.log(divs)
                 return divs
             } else {
-                return null
+                return []
             }
-        })
+        }).startWith([])
 
     const smallBtnStyle = bgcolor => ({
         style : {
@@ -109,25 +111,71 @@ export function Table(sources) {
             fontWeight : 'lighter'}
     })
 
-    const vdom$ = xs.combine(sampleTable.DOM, data$, props$, filterText$)
-            .map(([dom, data, props, filterText]) => div([
-                    div('.row .valign-wrapper', {style : {'margin-bottom' : '0px', 'padding-top' : '5px', 'background-color': props.color}}, [
-                        h5('.white-text .col .s5 .valign', props.title),
-                        div('.white-text .col .s7 .valign .right-align', filterText)
-                        // p('.white-text .col .s2', [ filterText ])
-                        // i('.Add .white-text .material-icons', 'playlist_add')
-                    ]),
-                    div('.row', {style : {'margin-bottom' : '0px', 'margin-top' : '0px'}}, [
-                        dom,
-                        div('.col .s12 .right-align', [
-                            button('.min5 .btn-floating waves-effect waves-light', smallBtnStyle(props.color), [i('.material-icons', 'fast_rewind')]),
-                            button('.plus5 .btn-floating waves-effect waves-light', smallBtnStyle(props.color), [i('.material-icons', 'fast_forward')])
-                        ])
-                    ]),
-                    // div('.row', [
-                    // ])
-                ])
-            ).startWith(div([]))
+    // 3 states: empty - loading - filled
+    // empty: startWith covers this
+    // 2 states remaining: loading - filled
+    // loading is between query and result
+    // []-------Request (start animation) oooooooRa
+    // two vdom streams and between/notbetween and merge/flatten?
+
+    /**
+     * source: --a--b----c----d---e-f--g----h---i--j-----
+     * first:  -------F------------------F---------------
+     * second: -----------------S-----------------S------
+     *                         between
+     * output: ----------c----d-------------h---i--------
+     */
+    function between(first, second) {
+        return (source) => first.mapTo(source.endWhen(second)).flatten()
+    }
+
+    // Keeping track of when an HTTP request is ongoing...
+    const loadingVdom$ = xs.combine(
+                                state$, 
+                                // xs.periodic(1000), 
+                                sampleTable.DOM, 
+                                data$.startWith([]), 
+                                props$, 
+                                filterText$
+                            )
+                            .map(([state, dom, data, props, filterText]) => div([
+                                        div('.row .valign-wrapper', {style : {'margin-bottom' : '0px', 'padding-top' : '5px', 'background-color': props.color}}, [
+                                            h5('.white-text .col .s5 .valign', props.title),
+                                            div('.white-text .col .s7 .valign .right-align', filterText)
+                                        ]),
+                                        div('.progress ', [div('.indeterminate')])
+                                    ]),
+                                )
+                            .compose(between(request$, data$))
+    // Show table when query is not in progress
+    const renderVdom$ = xs.combine(
+                                state$, 
+                                // xs.periodic(1000), 
+                                sampleTable.DOM, 
+                                data$.startWith([]), 
+                                props$, 
+                                filterText$
+                            )
+                            .map(([state, dom, data, props, filterText]) => div([
+                                        div('.row .valign-wrapper', {style : {'margin-bottom' : '0px', 'padding-top' : '5px', 'background-color': props.color}}, [
+                                            h5('.white-text .col .s5 .valign', props.title),
+                                            div('.white-text .col .s7 .valign .right-align', filterText)
+                                        ]),
+                                        div('.row', {style : {'margin-bottom' : '0px', 'margin-top' : '0px'}}, [
+                                            dom,
+                                            div('.col .s12 .right-align', [
+                                                button('.min5 .btn-floating waves-effect waves-light', smallBtnStyle(props.color), [i('.material-icons', 'fast_rewind')]),
+                                                button('.plus5 .btn-floating waves-effect waves-light', smallBtnStyle(props.color), [i('.material-icons', 'fast_forward')])
+                                            ])
+                                        ]),
+                                    ])
+                                )
+                            .compose(between(data$, request$))
+                            .startWith(div([]))      // Initial state!!!!
+
+    const vdom$ = xs.merge(renderVdom$, loadingVdom$)
+
+    // const vdom$ = renderVdom$ //, loadingVdom$).flatten()//.startWith(div())
 
     // Make sure that the state is cycled in order for SampleTable to pick it up
     const stateReducer$ = data$.map(data => prevState => {
@@ -142,7 +190,7 @@ export function Table(sources) {
 
   return { 
     	DOM: vdom$,
-        HTTP: request$,
+        HTTP: request$.compose(debounce(5000)),
         onion: reducer$
   };
 

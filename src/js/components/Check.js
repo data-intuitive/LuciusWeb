9d4abe7b3a3dfb6abcd3eb8c3efdd5a2f19ddd7f
Toni Verbeiren <toni.verbeiren@gmail.com>
2017-05-29 15:25:13 +0200

    Status indicator in Home

---
+++ src/js/components/Check.js
@@ -0,0 +1,93 @@
+import { a, div, br, label, input, p, button, code, pre, i, span } from '@cycle/dom'
+import xs from 'xstream'
+import isolate from '@cycle/isolate'
+import { mergeWith, merge } from 'ramda'
+import { clone, equal, equals, mergeAll, omit } from 'ramda';
+import dropRepeats from 'xstream/extra/dropRepeats'
+import debounce from 'xstream/extra/debounce'
+
+function Check(sources) {
+
+    const state$ = sources.onion.state$
+
+    const props$ = sources.props.debug()
+
+   const modifiedState$ = state$
+        .compose(dropRepeats((x, y) => equals(omit(['result'], x), omit(['result'], y))))
+        .compose(debounce(2000))
+        .debug()
+
+    const request$ = xs.combine(modifiedState$, props$)
+        .map(([state, props]) => {
+            return {
+                url: props.url + '&classPath=com.dataintuitive.luciusapi.statistics',
+                method: 'POST',
+                send: {},
+                'category': 'test'
+            }
+        })
+        .debug();
+
+    const response$$ = sources.HTTP
+        .select('test')
+
+    const invalidResponse$ = response$$
+        .map(response$ =>
+            response$
+                .filter(response => false) // ignore regular event
+                .replaceError(error => xs.of(error)) // emit error
+        )
+        .flatten()
+        // .compose(debounce(2000))
+        .debug()
+
+    const validResponse$ = response$$
+        .map(response$ =>
+            response$
+                .replaceError(error => xs.empty())
+        )
+        .flatten()
+        // .compose(debounce(2000))
+        .debug()
+
+    const data$ = validResponse$
+        .map(result => result.body.result.data)
+    .debug()
+
+    const initVdom$ = xs.of('...')
+
+    const loadingVdom$ = request$
+        .mapTo('...')
+
+    const loadedVdom$ = data$
+        .mapTo(i('.material-icons .green-text .medium', 'done'))
+
+    const errorVdom$ = invalidResponse$.mapTo(i('.material-icons .red-text .medium', 'trending_down'))
+
+    const vdom$ = xs.merge(
+        initVdom$,
+        loadingVdom$,
+        loadedVdom$,
+        errorVdom$,
+    ).remember()
+
+    // This is needed in order to get the onion stream active!
+    const defaultReducer$ = xs.of(prevState => {
+        if (typeof prevState === 'undefined') {
+            return {}
+        } else {
+            return prevState
+        }
+    });
+
+    return {
+        DOM: vdom$.debug(),
+        HTTP: request$,
+        onion: xs.merge(
+            defaultReducer$,
+        )
+
+    }
+}
+
+export { Check }
--- src/js/components/Router/index.js
+++ src/js/components/Router/index.js
@@ -1,18 +1,20 @@
 import xs from 'xstream';
 import { div, nav, a, h3, p, ul, li, h1, h2, i, footer, header, main, svg, g, path } from '@cycle/dom';
-import { merge, prop } from 'ramda';
+import { merge, prop, equals } from 'ramda';
 import BMI from '../../examples/bmi';
 import Hello from '../../examples/hello-world';
 import { HttpRequest } from "../../examples/http-request"
 import SignatureWorkflow from '../../pages/signature'
 import CompoundWorkflow from '../../pages/compound'
 import StatisticsWorkflow from '../../pages/statistics'
+import { Check } from '../Check'
 import { IsolatedSettings } from '../../pages/settings'
 import flattenSequentially from 'xstream/extra/flattenSequentially'
 import { pick, mix } from 'cycle-onionify';
 import { initSettings } from '../../pages/settings'
 import debounce from 'xstream/extra/debounce'
 import dropRepeats from 'xstream/extra/dropRepeats'
+
 function TargetWorkflow(sources) {
 
   const vdom$ = xs.of(
@@ -37,35 +39,47 @@ function TargetWorkflow(sources) {
 }
 
 function Home(sources) {
-  const vdom$ = xs.of(div('.row', [
-    h2('.col .s6 .offset-s3', 'Welcome to ComPass !'),
-    p('.col .s6 .offset-s3 .flow-text', [
-      'This application is the interface with L1000 data. Currently, ',
-      'there is support for working with disease profiles expressed using gene lists or signatures and compound similarity.'
-    ]),
-    div('.col .s6 .offset-s3', [
-      div('.col .s12 .pink .darken-4', { style: { padding: '10px 10px 10px 10px' } },
-        [
-          i('.pink-text .text-lighten-1 .material-icons', 'play_arrow'),
-          a('.pink-text .text-lighten-3', { props: { href: '/disease' }, style: { fontWeight: 'bolder', 'font-size': '32px' } }, ' Disease Workflow')
-        ]),
-      div('.row', []),
-      div('.col .s12 .orange .darken-4 .pink-text', { style: { padding: '10px 10px 10px 10px' } },
-        [
-          i('.orange-text .text-lighten-1 .material-icons', 'play_arrow'),
-          a('.orange-text .text-lighten-3', { props: { href: '/compound' }, style: { fontWeight: 'bolder', 'font-size': '32px' } }, ' Compound Workflow')
-        ]),
-      div('.row', []),
-      div('.col .s12 .red .darken-4 .pink-text', { style: { padding: '10px 10px 10px 10px' } },
-        [
-          i('.red-text .text-lighten-1 .material-icons', 'play_arrow'),
-          a('.red-text .text-lighten-4', { props: { href: '/target' }, style: { fontWeight: 'bolder', 'font-size': '32px' } }, ' Target Workflow')
-        ]),
-    ]),
-    p('.col .s6 .offset-s3 .flow-text', [
-      'You can click on one of the workflows above to start it.'
-    ]),
-  ]));
+
+  const checkProps$ = sources.onion.state$
+    .compose(dropRepeats((x, y) => equals(x.settings, y.settings)))
+    .startWith({ settings: initSettings })
+    .map(state => merge(state.settings.form, state.settings.api))
+  const CheckSink = Check(merge(sources, { props: checkProps$ }))
+
+  const vdom$ = xs.combine(CheckSink.DOM, xs.of(''))
+    .map(([check, r]) => div('.row', [
+      h2('.col .s6 offset-s3', ['Welcome to ComPass', check]),
+      p('.col .s6 .offset-s3 .flow-text', [
+        'This application is the interface with L1000 data. Currently, ',
+        'there is support for working with disease profiles expressed using gene lists or signatures and compound similarity.'
+      ]),
+      div('.col .s6 .offset-s3', [
+        div('.col .s12 .pink .darken-4', { style: { padding: '10px 10px 10px 10px' } },
+          [
+            i('.pink-text .text-lighten-1 .material-icons', 'play_arrow'),
+            a('.pink-text .text-lighten-3', { props: { href: '/disease' }, style: { fontWeight: 'bolder', 'font-size': '32px' } }, ' Disease Workflow')
+          ]),
+        div('.row', []),
+        div('.col .s12 .orange .darken-4 .pink-text', { style: { padding: '10px 10px 10px 10px' } },
+          [
+            i('.orange-text .text-lighten-1 .material-icons', 'play_arrow'),
+            a('.orange-text .text-lighten-3', { props: { href: '/compound' }, style: { fontWeight: 'bolder', 'font-size': '32px' } }, ' Compound Workflow')
+          ]),
+        div('.row', []),
+        div('.col .s12 .red .darken-4 .pink-text', { style: { padding: '10px 10px 10px 10px' } },
+          [
+            i('.red-text .text-lighten-1 .material-icons', 'play_arrow'),
+            a('.red-text .text-lighten-4', { props: { href: '/target' }, style: { fontWeight: 'bolder', 'font-size': '32px' } }, ' Target Workflow')
+          ]),
+      ]),
+      p('.col .s6 .offset-s3 .flow-text', [
+        'You can click on one of the workflows above to start it.'
+      ]),
+      // p('.col .s6 .offset-s3', [
+      //   'Interface status: ', 
+      //   check
+      // ]),
+    ]));
 
   const router$ = sources.DOM.select('a').events('click')
     .debug(ev => ev.preventDefault())
@@ -73,7 +87,9 @@ function Home(sources) {
 
   return {
     DOM: vdom$,
-    router: router$
+    router: router$,
+    HTTP: CheckSink.HTTP,
+    onion: CheckSink.onion
   };
 }
 
@@ -85,8 +101,6 @@ export default function Router(sources) {
     console.log(state)
   });
 
-  console.log(router)
-
   const match$ = router.define({
     '/': Home,
     '/disease': SignatureWorkflow,
@@ -94,19 +108,21 @@ export default function Router(sources) {
     '/target': TargetWorkflow,
     '/statistics': StatisticsWorkflow,
     '/settings': IsolatedSettings,
+    '*': Home
     // '/bmi': BMI,
     // '/hello': Hello,
     // '/http': HttpRequest,
-    '*': Home
   })
-  .remember();
+    .remember();
 
-  const page$ = match$.map(({path, value}) => {
+  const page$ = match$.map(({ path, value }) => {
     return value(Object.assign({}, sources, {
       router: sources.router.path(path)
     }))
   })
-  .remember()
+    // .compose(dropRepeats((x,y) => equal(x,y)))
+    .debug('-------------PAGE-----------')
+    .remember()
 
   const makeLink = (path, label) => li([a({ props: { href: path } }, label)]);
 
@@ -127,12 +143,12 @@ export default function Router(sources) {
   ])
   ]));
 
+
   const footer$ = xs.of(
     footer('.page-footer .grey .darken-4 .grey-text', [
       div('.row', [
         div('.col .s12', [
           p(['Please use ', a({ props: { href: '/statistics' } }, 'the information'), ' provided in ComPass with care. ComPass does not make any claims.']),
-          p(['General statistics: ', ]),
         ]),
       ]),
       div('.footer-copyright .row', [
@@ -165,7 +181,7 @@ export default function Router(sources) {
   });
 
   return {
-    DOM: vdom$, 
+    DOM: vdom$,
     router: page$.map(c => c.router || xs.never()).flatten(),
     HTTP: page$.map(prop('HTTP')).filter(Boolean).flatten(),
     onion: xs.merge(

a059878e9b65b39c6b4a155517098c1bfa91fa33
Toni Verbeiren <toni.verbeiren@gmail.com>
2017-05-29 09:54:07 +0200

    Removed some debug() statements

--- src/js/pages/settings.js
+++ src/js/pages/settings.js
@@ -173,7 +173,7 @@ export function Settings(sources) {
 	const headTableSettings$ = xs.combine(settings$.map(state => state.headTableSettings), headTableCount$, headColor$)
 		.map((settings) => ({
 			headTableSettings: mergeAll(settings),
-		})).debug()
+		}))
 
 	const tailTableSettings$ = xs.combine(settings$.map(state => state.tailTableSettings), tailTableCount$, tailColor$)
 		.map((settings) => ({

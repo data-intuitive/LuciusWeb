18b1a27a6c288bf782368ebfcc38dc0fe69d1ccc
Toni Verbeiren <toni.verbeiren@gmail.com>
2017-07-30 19:49:45 +0200

    SampleSelection: towards standardizing component flow

--- src/js/components/SampleSelection.js
+++ src/js/components/SampleSelection.js
@@ -16,24 +16,62 @@ const emptyData = {
         }
     }
 }
+
+const sampleSelectionLens = { 
+    get: state => ({sampleSelection: state.form.sampleSelection, settings: state.settings}),
+    set: (state, childState) => ({...state, form: {...state.form, sampleSelection: childState.sampleSelection}})
+};
+
+/**
+ * Based on a (list of) compound(s), get the samples that correspond to it and allow users to select them.
+ * 
+ * input: compound(s) (string)
+ * output: list of samples (array)
+ */
 function SampleSelection(sources) {
 
-    const state$ = sources.onion.state$.debug()
+    const state$ = sources.onion.state$.debug(state => {
+        console.log('== State in SampleSelection =================')
+        console.log(state)
+    });
 
-    const compoundQuery$ = sources.query//.startWith('1211912 123175')
+    const input$ = sources.input
 
-    const request$ = xs.combine(compoundQuery$, sources.props)
-        .map(([query, props]) => {
+    const emptyState$ = state$
+         .filter(state => state.sampleSelection.input == null || state.sampleSelection.input == '')
+         .compose(dropRepeats((x, y) => equals(x, y)))
+
+    // When the state is cycled because of an internal update
+    const modifiedState$ = 	state$
+        .filter(state => state.sampleSelection.input != '')
+        .compose(dropRepeats((x,y) => equals(x,y)))
+        // .debug()
+
+    const newInput$ = xs.combine(
+            input$, 
+            state$
+        )
+        .map(([newinput, state]) => ({...state, sampleSelection: {...state.sampleSelection, input: newinput}}))
+        .compose(dropRepeats((x,y) => equals(x.sampleSelection.input,y.sampleSelection.input)))
+        // .debug()
+
+    // When a new query is required
+    const updatedState$ = state$
+		.compose(dropRepeats((x, y) => equals(x.sampleSelection, y.sampleSelection)))
+        .debug()
+
+    const request$ = newInput$
+        .map(state => {
             return {
-                url: props.url + '&classPath=com.dataintuitive.luciusapi.samples',
+                url: state.settings.api.url + '&classPath=com.dataintuitive.luciusapi.samples',
                 method: 'POST',
                 send: {
                     version: 'v2',
-                    query: query
+                    query: state.sampleSelection.input
                 },
                 'category': 'samples'
             }
-        })
+        }).debug()
 
     const response$ = sources.HTTP
         .select('samples')
@@ -45,9 +83,7 @@ function SampleSelection(sources) {
 
     const data$ = response$
         .map(res => res.body)
-        // .startWith(emptyData.body)
         .map(json => json.result.data)
-        .debug()
 
     const useClick$ = sources.DOM
         .select('.selection')
@@ -57,6 +93,7 @@ function SampleSelection(sources) {
 
     // Helper function for rendering the table, based on the state
     const makeTable = (data) => {
+        console.log(data)
         let rows = data.map(entry => [
             td(entry.jnjs),
             td((entry.compoundname.length > 10) ? entry.compoundname.substring(0, 10) : entry.compoundname),
@@ -99,42 +136,57 @@ function SampleSelection(sources) {
         );
     }
 
-    const vdom$ = state$
-        .map((data) => makeTable(data))
+    const initVdom$ = emptyState$.mapTo(div())
 
-    const defaultReducer$ = data$.map(data => prevState => {
-        console.log('Default reducer')
-        return data.map(el => merge(el, { use: true }))
+    const loadingVdom$ = xs.combine(request$, modifiedState$).mapTo(div())
+
+    const loadedVdom$ = modifiedState$
+        .map(state => makeTable(state.sampleSelection.data))
+
+    const vdom$ = xs.merge(initVdom$, loadingVdom$, loadedVdom$)
+
+    const dataReducer$ = data$.map(data => prevState => {
+        const newData = data.map(el => merge(el, { use: true }))
+        return {...prevState, sampleSelection: {...prevState.sampleSelection, 
+            data: newData,
+            output: newData.filter(x => x.use).map(x => x.id) }}
     })
 
     const selectReducer$ = useClick$.map(id => prevState => {
-        var newState = clone(prevState)
-        return newState.map(el => {
-            // One sample object
-            var newEl = clone(el)
-            const switchUse = (id === el.id)
-            newEl.use = (switchUse) ? !el.use : el.use
-            return newEl
-        })
-
+        const newData = prevState.sampleSelection.data.map(el => {
+                    // One sample object
+                    var newEl = clone(el)
+                    const switchUse = (id === el.id)
+                    newEl.use = (switchUse) ? !el.use : el.use
+                    return newEl
+                })
+        return ({...prevState, 
+            sampleSelection: {...prevState.sampleSelection,
+                data: newData,
+                output: newData.filter(x => x.use).map(x => x.id) 
+        }})
     })
 
-    const sampleSelection$ = sources.DOM.select('.doSelect').events('click')
+   const defaultReducer$ = xs.of(prevState => ({...prevState, sampleSelection: {input: '', data: []}}))
+   const inputReducer$ = input$.map(i => prevState => ({...prevState, sampleSelection: {...prevState.sampleSelection, input: i}}))
+   const requestReducer$ = request$.map(req => prevState => ({...prevState, sampleSelection: {...prevState.sampleSelection, request: req}}))
+
+   const sampleSelection$ = sources.DOM.select('.doSelect').events('click')
         .compose(sampleCombine(state$))
-        .debug()
-        .map(([ev, state]) => state.filter(x => x.use).map(x => x.id))
-        .debug()
+        .map(([ev, state]) => state.sampleSelection.output)
 
     return {
         DOM: vdom$,
-        selection: sampleSelection$,
-        HTTP: request$,
+        HTTP: request$,//.compose(debounce(2000)),
         onion: xs.merge(
             defaultReducer$,
+            inputReducer$,
+            requestReducer$,
+            dataReducer$,
             selectReducer$
-        )
+        ),
+        output: sampleSelection$,
     }
-
 }
 
-export { SampleSelection }
+export { SampleSelection, sampleSelectionLens }

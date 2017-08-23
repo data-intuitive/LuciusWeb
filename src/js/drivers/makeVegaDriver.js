42a2015afec217f89c4f197faccf27540f8e8b95
Toni Verbeiren <toni.verbeiren@gmail.com>
2017-08-23 15:57:25 +0200

    Cleaned up vegaDriver

--- src/js/drivers/makeVegaDriver.js
+++ src/js/drivers/makeVegaDriver.js
@@ -1,81 +1,34 @@
 import xs from 'xstream'
 import { View } from 'vega-view'
 import { Warn } from 'vega'
+import { parse } from 'vega-parser'
 
 function makeVegaDriver() {
 
-  function vegaDriver(spec$) {
+    function vegaDriver(spec$) {
 
-    spec$.addListener({
-        next: (spec) => new View(spec.runtime).width(spec.width).height(350).logLevel(Warn).renderer('canvas').initialize(spec.el).hover().run(),
-    })
+        const view$ = spec$
+            .map(specInfo => (
+                {
+                    view: new View(specInfo.runtime).width(specInfo.width).height(350).logLevel(Warn),
+                    el: specInfo.el
+                }
+            ))
 
-    // return result$
+        view$.addListener({
+            next: (viewInfo) => {
+                viewInfo.view.initialize(viewInfo.el).hover().run()
+                console.log('--> new view created')
+            },
+            error: (m) => {
+                console.error(m)
+            }
+        })
 
-    // const view$$ = spec$.map(
-    //     (obj) => {
+    }
+    
+    return vegaDriver
 
-    //         // const parsed$ = xs.create({
-    //         //     start: (listener) => {
-    //         //        vg.parse(obj.spec, res => listener.next(res))
-    //         //     }, 
-    //         //     stop: () => {}
-    //         // })
-            
-    //         const view$ = parsed$.map(chart => vg.View({el:obj.el}).width(obj.width).height(350).update())
-
-    //         const errorHandlingView$ = view$.replaceError(() => xs.empty())
-
-    //         return errorHandlingView$
-
-    //     }
-    // );
-
-    // const view$ = view$$.flatten();
-
-    // const click$$ = view$.map(view => {
-
-    //     const added$ = xs.create({
-    //         start: (listener) => {
-    //             view.on('click', function(event, item) {
-    //                 listener.next({obj: view.el, data: item.datum});
-    //             }
-    //         )},
-    //         stop: () => {}
-    //     })
-
-    //     return added$
-
-    // });
-
-    // const click$ = click$$.flatten()
-
-    // view$.addListener({
-    //     next: (n) => {},
-    //     error: (e) => {
-    //         console.error('An error occured in the vegaDriver')
-    //         console.error(e)
-    //     },
-    //     complete: () => {}            
-    // });
-
-    // click$.addListener({
-    //     next: (n) => {
-    //         console.log(n)
-    //     },
-    //     error: (e) => {
-    //         console.error('An error occured in the click stream')
-    //         console.error(e)
-    //     },
-    //     complete: () => {}            
-    // });
-
-    // return click$;
-
-  }
-
-  // We return the actual driver from our factory.
-  return vegaDriver
 }
 
-export {makeVegaDriver};
+export { makeVegaDriver };

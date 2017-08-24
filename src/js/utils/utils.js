6cc0823fc23c591ab2ee6c4ea0d75382b4725c00
Toni Verbeiren <toni.verbeiren@gmail.com>
2017-08-24 10:15:28 +0200

    Cleanup

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,16 +1,6 @@
 import dropRepeats from 'xstream/extra/dropRepeats'
 import { prop } from 'ramda'
 
-// const log = (x) => console.log(x);
-
-export const stateDebug = component => state => {
-	if (prop(component, state.settings).DEBUG == true) {
-		console.log('== State in <<' + component + '>>')
-		console.log(state)	
-	} else {
-	}
-}
-
 // Size stream, make it dependent on the size of container which is managed by CSS.
 // TODO: Make it update immediately, currently only updates on new query
 export function widthStream(domSource$, el) {
@@ -29,15 +19,3 @@ export function widthStream(domSource$, el) {
                 .remember()
                 // .debug(log)
 }
-
-/**
- * source: --a--b----c----d---e-f--g----h---i--j-----
- * first:  -------F------------------F---------------
- * second: -----------------S-----------------S------
- *                         between
- * output: ----------c----d-------------h---i--------
- */
-export function between(first, second) {
-    return (source) => first.mapTo(source.endWhen(second)).flatten()
-}
-

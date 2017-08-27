c3fc945bf6f042391044d95fd145194b8bea5159
Toni Verbeiren <toni.verbeiren@gmail.com>
2017-08-27 18:52:54 +0200

    Improved statistics page

--- src/js/components/Statistics.js
+++ src/js/components/Statistics.js
@@ -1,4 +1,4 @@
-import { a, div, br, label, input, p, button, code, pre, span } from '@cycle/dom'
+import { a, div, br, label, input, p, button, code, pre, span, h5 } from '@cycle/dom'
 import xs from 'xstream'
 import isolate from '@cycle/isolate'
 import { mergeWith, merge } from 'ramda'
@@ -41,8 +41,6 @@ function Statistics(sources) {
                 .replaceError(error => xs.of(error)) // emit error
         )
         .flatten()
-        // .compose(debounce(2000))
-        .debug()
 
     const validResponse$ = response$$
         .map(response$ =>
@@ -50,23 +48,18 @@ function Statistics(sources) {
                 .replaceError(error => xs.empty())
         )
         .flatten()
-        // .compose(debounce(2000))
-        .debug()
 
     const data$ = validResponse$
         .map(result => result.body.result.data)
-    // .debug()
 
 
     const container = (el) => div([
         div('.row', [
-            div('.col .s6 .offset-s3', [
-                div('.card .grey .lighten-2', [
-                    div('.card-content', [
-                        span('.card-title', ['Statistics:']),
-                        span(el)
-                    ])
-                ])
+            div('.col .s10 .offset-s1', [
+                div('.row', []),
+                h5('.center-align', ['Statistics']),
+                div('.row', []),
+                div('.green-text .text-darken-3', { style: { fontSize: 'large' } }, el)
             ])
         ])
     ])
@@ -78,11 +71,40 @@ function Statistics(sources) {
 
     const loadedVdom$ = data$
         .map(data => container([
-            p(['# Compounds: ', data.compounds]),
-            p(['# Samples: ', data.samples]),
-            p(['# Genes: ', data.genes]),
+
+            // div('.row', [
+            div('.col .s3 .green .darken-3 .center-align', [p('.green-text .text-darken-3', { style: { fontSize: 'large' } }, ['nothing'])]),
+            div('.col .s3 .green .darken-3 .center-align', [p('.white-text', { style: { fontSize: 'huge' } }, ['MCF7'])]),
+            div('.col .s3 .green .darken-3 .center-align', [p('.white-text', { style: { fontSize: 'large' } }, ['PBMC'])]),
+            div('.col .s3 .green .darken-3 .center-align', [p('.white-text', { style: { fontSize: 'large' } }, ['Total'])]),
+
+            // ]),
+            // div('.row', [
+            div('.col .s3', [p(['# profiles'])]),
+            div('.col .s3 .center-align', [p(data.samples.mcf7)]),
+            div('.col .s3 .center-align', [p(data.samples.pbmc)]),
+            div('.col .s3 .center-align', [p(data.samples.total)]),
+
+            // ]),
+            // div('.row', [
+            div('.col .s3', [p(['# informative profiles'])]),
+            div('.col .s3 .center-align', [p(data.informative.mcf7)]),
+            div('.col .s3 .center-align', [p(data.informative.pbmc)]),
+            div('.col .s3 .center-align', [p(data.informative.total)]),
+
+            // ]),
+            // div('.row', [
+            div('.col .s3', [p(['# unique compounds'])]),
+            div('.col .s3 .center-align', [p(data.compounds.mcf7)]),
+            div('.col .s3 .center-align', [p(data.compounds.pbmc)]),
+            div('.col .s3 .center-align', [p(data.compounds.total)]),
+
+            // ])
+            // p(['# Compounds: ', data.compounds]),
+            // p(['# Samples: ', data.samples]),
+            // p(['# Genes: ', data.genes]),
         ])
-    )
+        )
 
     const errorVdom$ = invalidResponse$.mapTo(div([p('An error occured !!!')]))
 
@@ -91,7 +113,7 @@ function Statistics(sources) {
         loadingVdom$,
         loadedVdom$,
         errorVdom$,
-    ).remember()
+    )
 
     // This is needed in order to get the onion stream active!
     const defaultReducer$ = xs.of(prevState => {

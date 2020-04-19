de6b4c9074b59605aa3203b632ee075c5ea491df
Toni Verbeiren <toni.verbeiren@gmail.com>
2020-04-19 15:56:33 +0200

    Cleanup

--- src/js/pages/statistics.js
+++ src/js/pages/statistics.js
@@ -12,66 +12,66 @@ import { Statistics } from '../components/Statistics'
 
 function StatisticsWorkflow(sources) {
 
-    const state$ = sources.onion.state$
+  const state$ = sources.onion.state$
 
-	// Initialize if not yet done in parent (i.e. router) component (useful for testing)
-	const defaultReducer$ = xs.of(prevState => {
-		if (typeof prevState === 'undefined') {
-			return { settings: initSettings }
-		} else {
-			return prevState
-		}
-	})
+  // Initialize if not yet done in parent (i.e. router) component (useful for testing)
+  const defaultReducer$ = xs.of(prevState => {
+    if (typeof prevState === 'undefined') {
+      return { settings: initSettings }
+    } else {
+      return prevState
+    }
+  })
 
-	// Create a stream with settings and properties based on the
-	// settings keys provided. settings lives under state.
-	function selectKeysFromSettings(thisState$, keys) {
-		return state$
-			.map(prop('settings'))
-			.map(props(keys))
-			.map(mergeAll)
-			.compose(dropRepeats((x, y) => equals(x, y)))
-	}
+  // Create a stream with settings and properties based on the
+  // settings keys provided. settings lives under state.
+  function selectKeysFromSettings(thisState$, keys) {
+    return state$
+      .map(prop('settings'))
+      .map(props(keys))
+      .map(mergeAll)
+      .compose(dropRepeats((x, y) => equals(x, y)))
+  }
 
-	// Add properties to the child component, based on what we need
-	const statsProps$ = selectKeysFromSettings(state$, ['stats', 'api'])
-	const statsSinks = isolate(Statistics, 'stats')(merge(sources, { props: statsProps$ }));
+  // Add properties to the child component, based on what we need
+  const statsProps$ = selectKeysFromSettings(state$, ['stats', 'api'])
+  const statsSinks = isolate(Statistics, 'stats')(merge(sources, { props: statsProps$ }));
 
-	const pageStyle = {
-		style:
-		{
-			fontSize: '14px',
-			opacity: '0',
-			transition: 'opacity 1s',
-			delayed: { opacity: '1' },
-			destroy: { opacity: '0' }
-		}
-	}
+  const pageStyle = {
+    style:
+    {
+      fontSize: '14px',
+      opacity: '0',
+      transition: 'opacity 1s',
+      delayed: { opacity: '1' },
+      destroy: { opacity: '0' }
+    }
+  }
 
-	const vdom$ = xs.combine(
-		statsSinks.DOM,
-	)
-		.map(([
-			stats
-		]) =>
-			div('.row .grey .lighten-5  ', [
-				div('.col .s10 .offset-s1', pageStyle,
-					[
-						div('.col .s12', [stats]),
-					])
-			])
-		);
+  const vdom$ = xs.combine(
+    statsSinks.DOM,
+  )
+    .map(([
+      stats
+    ]) =>
+      div('.row .grey .lighten-5  ', [
+        div('.col .s10 .offset-s1', pageStyle,
+          [
+            div('.col .s12', [stats]),
+          ])
+      ])
+    );
 
-	return {
-		DOM: vdom$,
-		onion: xs.merge(
-			defaultReducer$,
-			statsSinks.onion,
-		),
-		HTTP: xs.merge(
-			statsSinks.HTTP,
-		),
-	};
+  return {
+    DOM: vdom$,
+    onion: xs.merge(
+      defaultReducer$,
+      statsSinks.onion,
+    ),
+    HTTP: xs.merge(
+      statsSinks.HTTP,
+    ),
+  };
 }
 
 export default StatisticsWorkflow;

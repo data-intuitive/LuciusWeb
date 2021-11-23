94845e809d926121901c1b57593bb228609193f4
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2021-11-23 09:21:53 +0100

    Fix exporting of TSVs
    Convert filters array of objects to an array of strings, which can be serialized

--- src/js/utils/export.js
+++ src/js/utils/export.js
@@ -8,6 +8,29 @@ const convertToSafeString = (s) => {
 }
 
 function convertToCSV(objArray) {
+
+    // input is array of objects with 'key' and 'value' members
+    // returning array of strings in format 'key:value'. Javascript handles serializing the arrays for us.
+    const filterStringify = (filters) => 
+        {
+            return filters
+                    .map((f) => {
+                        return convertToSafeString(f.key) + ':' + convertToSafeString(f.value)
+                        })
+        }
+    
+    // only change filters if the member exists
+    const data = objArray.map(
+                    (d) => (d.filters !== undefined ? 
+                                {...d, filters: filterStringify(d.filters)} : 
+                                d
+                            )
+                        )
+
+    return convertToCSV_internal(data)
+}
+
+function convertToCSV_internal(objArray) {
     const header = keys(objArray[0])
     const data = objArray.map(obj => values(obj))
 

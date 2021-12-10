7814079221786fed4e0b488c33e4eac8ecefad20
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2021-12-10 11:40:24 +0100

    Add documentation for Settings page
    Most bottom level functions are documented, top level still to do

--- src/js/pages/settings.js
+++ src/js/pages/settings.js
@@ -14,13 +14,43 @@ import { mergeWith, merge, mergeAll } from "ramda"
 import { pick, mix } from "cycle-onionify"
 import debounce from "xstream/extra/debounce"
 
+/**
+ * @module pages/Settings
+ */
+
+/**
+ * Isolate the Settings component/page
+ * @function IsolatedSettings
+ * @param {Stream} sources 
+ * @returns Isolated Settings component/page
+ */
 export function IsolatedSettings(sources) {
   return isolate(Settings, "settings")(sources)
 }
 
+/**
+ * Create a component/page that allows configuring the application
+ * @function Settings
+ * @param {Stream} sources 
+ * @returns Settings component/page
+ */
 export function Settings(sources) {
   const settings$ = sources.onion.state$
 
+  /**
+   * Settings to display on the page
+   * 'group' has to match to the member name in state$ -> settings.'group'
+   * 'title' is text displayed on the page for this group
+   * 'settings' contains information for each nested field
+   *    'field'   has to match to the member name in state$ -> setting.'group'.'field'
+   *    'type'    text field determining how the value will be displayed, text, checkbox or range
+   *    'class'   identifier for the field vdom div
+   *    'title'   is text displayed on the page for this field
+   *    'options' possible values for a multiple choice field
+   *    'props'   extra properties to pass to the input field
+   * @const Settings/settingsConfig
+   * @type {Array(Object)}
+   */
   const settingsConfig = [
     {
       group: "common",
@@ -125,9 +155,28 @@ export function Settings(sources) {
     },
   ]
 
+  /**
+   * Display field setting, rendered according config.type
+   * @const Settings/makeSetting
+   * @param {Object} config description how the field should be displayed and options of e.g. minimum and maximum values
+   *    'type'    text field determining how the value will be displayed, text, checkbox or range
+   *    'class'   identifier for the field vdom div
+   *    'title'   is text displayed on the page for this field
+   *    'options' possible values for a multiple choice field
+   *    'props'   extra properties to pass to the input field
+   * @returns Object with
+   *            - onion: reducer to update the value to the new set value
+   *            - DOM: vdom stream of li and sub-elements
+   * @type {Object}
+   */
   const makeSetting = (config) => (sources) => {
     const state$ = sources.onion.state$
 
+    /**
+     * Stream of DOM updates, clicks for checkboxes or the new value for other input types
+     * @const Settings/makeSetting/update$
+     * @type {Stream}
+     */
     const update$ =
       config.type == "checkbox"
         ? sources.DOM.select("input")
@@ -135,6 +184,15 @@ export function Settings(sources) {
             .map((event) => event)
         : sources.DOM.events("input").map((event) => event.target.value)
 
+    /**
+     * @function Settings/makeSetting/renderField
+     * @param {Object} config description how the input field should be rendered or what options to give
+     *    'type'    text field determining how the value will be displayed, text, checkbox or range
+     *    'options' possible values for a multiple choice field
+     *    'props'   extra properties to pass to the input field
+     * @param {*} _state current value of the field
+     * @returns vdom element according specification in config
+     */ 
     function renderField(config, _state) {
       if (config.type == "checkbox") {
         return [
@@ -169,7 +227,11 @@ export function Settings(sources) {
       }
     }
 
-
+    /**
+     * Display field with title, value and input field depending on config
+     * @const Settings/makeSetting/vdom$
+     * @type {Stream}
+     */
     const vdom$ = state$.map((state) =>
       li(
         ".collection-item .row",
@@ -192,6 +254,11 @@ export function Settings(sources) {
       )
     )
 
+    /**
+     * Conditional reducer depending on field type
+     * @const Settings/makeSetting/updateReducer$
+     * @type {Reducer}
+     */
     const updateReducer$ =
       config.type == "checkbox"
         ? update$.map((_) => (prevState) => !prevState)
@@ -203,34 +270,80 @@ export function Settings(sources) {
     }
   }
 
-  const safelyMakeSetting$ = (setting, sources) => {
-
+  /**
+   * Perform sanity check before calling isolate(makeSetting(...),...)
+   * If we detect that the required member value is not present in onionified state, 
+   *    display static vdom with an error message instead.
+   * If isolate is called without the passed member is present in the onionified state, 
+   *    the whole system breaks down and we end up with an empty page without any error messages being displayed,
+   *    making it quite difficult to debug what setting is wrong.
+   * @const Settings/safelyMakeSetting$
+   * @param {Object} config description how the field should be displayed and options of e.g. minimum and maximum values
+   *    'field'   has to match to the member name in state$ -> setting.'group'.'field'
+   *    'type'    text field determining how the value will be displayed, text, checkbox or range
+   *    'class'   identifier for the field vdom div
+   *    'title'   is text displayed on the page for this field
+   *    'options' possible values for a multiple choice field
+   *    'props'   extra properties to pass to the input field
+   * @param {Stream} sources onionified state for the group this setting is in, and to be further onionified
+   * @returns Stream of Object with
+   *            - onion: reducer to update the value to the newly set value
+   *            - DOM: vdom stream of li and sub-elements
+   * @type {Stream(Object)}
+   */
+  const safelyMakeSetting$ = (config, sources) => {
+    /**
+     * Minimalistic vdom to be displayed when a field can't be found in the sources
+     * Mimics same member values as MakeSettings so they can be combined later
+     *    - DOM:   stream of fixed vdom li
+     *    - onion: empty stream instead of reducers
+     * @const Settings/safelyMakeSetting$/missingFieldVdom
+     * @type {Object}
+     */
     const missingFieldVdom = {
         DOM: xs.of(
           li(".collection-item .row",
             div(".valign-wrapper", [
-              span(".col .l6 .s12 .truncate .flow-text", [setting.title]),
-              span(".col .s6 .red-text", ["No value '" + setting.field + "' in configuration"]),
+              span(".col .l6 .s12 .truncate .flow-text", [config.title]),
+              span(".col .s6 .red-text", ["No value '" + config.field + "' in configuration"]),
             ])
           )),
         onion: xs.empty()
       }
 
+    /**
+     * Stream of either isolated MakeSetting in case field is found in state$ or static vdom in case field is missing in state$
+     * @const Settings/safelyMakeSetting$/vdom$
+     * @type {Stream(Object)}
+     */
     const vdom$ = sources.onion.state$
       .map((state) => (
-          (setting.field in state) ?
-            isolate(makeSetting(setting), setting.field)(sources) :
+          (config.field in state) ?
+            isolate(makeSetting(config), config.field)(sources) :
             missingFieldVdom
         )
     )
     return vdom$
   }
 
+  /**
+   * Display group header and create line of each field with name and input field
+   * @const Settings/settingsGroupObj
+   * @param {Object} settingsGroupObj Information how the group should be displayed:
+   *            - title: Name to be displayed
+   *            - settings: Array of Objects for fields to be displayed
+   * @returns Object with
+   *            - onion: reducers to update the field values to the newly set values
+   *            - DOM: vdom stream of ul and sub-elements
+   */
   const makeSettingsGroup = (settingsGroupObj) => (sources) => {
-    const group$ = sources.onion.state$
     const settingsArray = settingsGroupObj.settings
     const title = settingsGroupObj.title
 
+    /**
+     * Convert array of field settings to stream of objects contains DOM and reducers
+     * @const Settings/settingsGroupObj/components$
+     */
     const components$ = xs.of(settingsArray)
       .map((settings) =>
         settings.map((setting) =>
@@ -240,6 +353,11 @@ export function Settings(sources) {
       .compose(mix(xs.combine))
       .remember()
 
+    /**
+     * Combines all vdoms from the group field components and adds a title above them
+     * @const Settings/settingsGroupObj/vdom$
+     * @type {MemoryStream}
+     */
     const vdom$ = components$
       .compose(pick("DOM"))
       .compose(mix(xs.combine))
@@ -251,6 +369,11 @@ export function Settings(sources) {
       )
       .remember()
 
+    /**
+     * Combines all reducer streams from the group field components
+     * @const Settings/settingsGroupObj/reducer$
+     * @type {Stream}
+     */
     const reducer$ = components$.compose(pick("onion")).compose(mix(xs.merge))
 
     return {
@@ -258,8 +381,33 @@ export function Settings(sources) {
       DOM: vdom$,
     }
   }
-
+  /**
+   * Perform sanity check before calling isolate(makeSettingsGroup(...),...)
+   * If we detect that the required group value is not present in onionified state, 
+   *    display static vdom with an error message instead.
+   * If isolate is called without the passed member is present in the onionified state, 
+   *    the whole system breaks down and we end up with an empty page without any error messages being displayed,
+   *    making it quite difficult to debug what setting is wrong.
+   * @const Settings/safelyMakeSettingsGroup$
+   * @param {Object} group description how the group should be displayed
+   *            - 'group' has to match to the member name in state$ -> settings.'group'
+   *            - 'title' is text displayed on the page for this group
+   *            - 'settings' contains information for each nested field
+   * @param {Stream} sources onionified state of settings, and to be further onionified
+   * @returns Stream of Object with
+   *            - onion: reducers to update the field values to the newly set values
+   *            - DOM: vdom stream of ul and sub-elements
+   * @type {Stream(Object)}
+   */
   const safelyMakeSettingsGroup$ = (group, sources) => {
+    /**
+     * Minimalistic vdom to be displayed when a group can't be found in the sources
+     * Mimics same member values as MakeSettingsGroup so they can be combined later
+     *    - DOM:   stream of fixed vdom ul
+     *    - onion: empty stream instead of reducers
+     * @const Settings/safelyMakeSettingsGroup$/missingGroupVdom
+     * @type {Object}
+     */
     const missingGroupVdom = {
         DOM: xs.of(
           ul(
@@ -283,11 +431,9 @@ export function Settings(sources) {
   }
 
   const makeSettings = (settingsObj) => (sources) => {
-    const settings$ = sources.onion.state$
 
-    const groups$ = xs
-      .combine(xs.of(settingsObj), sources.onion.state$)
-      .map(([groups, state]) =>
+    const groups$ = xs.of(settingsObj)
+      .map((groups) =>
         groups.map((group) =>
           safelyMakeSettingsGroup$(group, sources)
         )

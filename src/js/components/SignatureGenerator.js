d0b1ff3f24e6cb80385572a237dc5d618a66a6d1
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2021-12-21 15:43:36 +0100

    Limit the amount of genes shown in the signature generator
    Add button to show the full signature, pressing again reduces the amount back to the limited view

--- src/js/components/SignatureGenerator.js
+++ src/js/components/SignatureGenerator.js
@@ -13,6 +13,10 @@ import { GeneAnnotationQuery } from './GeneAnnotationQuery'
 import { absGene } from '../utils/utils'
 import { busyUiReducer, dirtyWrapperStream } from "../utils/ui"
 
+/**
+ * @module components/SignatureGenerator
+ */
+
 const emptyData = {
     body: {
         result: {
@@ -28,18 +32,27 @@ const signatureLens = {
     set: (state, childState) => ({ ...state, form: { ...state.form, signature: childState.core } })
 };
 
-function model(newInput$, request$, data$) {
+function model(newInput$, request$, data$, showMore$) {
 
-        // Initialization
-        const defaultReducer$ = xs.of(prevState => ({ ...prevState, core: { input: '' } }))
-        // Add input to state
-        const inputReducer$ = newInput$.map(i => prevState => ({ ...prevState, core: { ...prevState.core, input: i } }))
-        // Add request body to state
-        const requestReducer$ = request$.map(req => prevState => ({ ...prevState, core: { ...prevState.core, request: req } }))
-        // Add data from API to state, update output key when relevant
-        const dataReducer$ = data$.map(newData => prevState => ({ ...prevState, core: { ...prevState.core, data: newData, output: newData.join(" ") } }))
-        // Logic and reducer stream that monitors if this component is busy
-        const busyReducer$ = busyUiReducer(newInput$, data$)
+    // Initialization
+    const defaultReducer$ = xs.of(prevState => ({ ...prevState, core: { input: '' } }))
+    // Add input to state
+    const inputReducer$ = newInput$.map(i => prevState => ({ ...prevState, core: { ...prevState.core, input: i } }))
+    // Add request body to state
+    const requestReducer$ = request$.map(req => prevState => ({ ...prevState, core: { ...prevState.core, request: req } }))
+    // Add data from API to state, update output key when relevant
+    const dataReducer$ = data$.map(newData => prevState => ({ ...prevState, core: { ...prevState.core, data: newData, output: newData.join(" ") } }))
+    // Logic and reducer stream that monitors if this component is busy
+    const busyReducer$ = busyUiReducer(newInput$, data$)
+    // Toggle how many signature genes can be displayed
+    const limitReducer$ = showMore$.map(showMore => prevState => ({
+        ...prevState, 
+        core: { 
+            ...prevState.core, 
+            showMore: showMore, 
+            showLimit: (showMore ? -1 : 100) 
+        }
+    }))
 
     return xs.merge(
         defaultReducer$,
@@ -47,9 +60,21 @@ function model(newInput$, request$, data$) {
         dataReducer$,
         requestReducer$,
         busyReducer$,
+        limitReducer$,
     )
 }
 
+/**
+ * SignatureGenerator view, display the component on the vdom
+ * @function view
+ * @param {Stream} state$ full state onion
+ * @param {Stream} request$ Trigger from API request to display the loading vdom
+ * @param {Stream} response$ Reply from API containing signature
+ * @param {Object} geneAnnotationQuery Object with DOM member to be added in vdom
+ * @returns Object with:
+ *          - vdom$: VNodes object
+ *          - signature$: stream of the processed signature
+ */
 function view(state$, request$, response$, geneAnnotationQuery) {
 
     const validSignature$ = response$
@@ -70,20 +95,69 @@ function view(state$, request$, response$, geneAnnotationQuery) {
         }
     }
 
+    /**
+     * Wrap a gene string into a div with styling
+     * @const view/showGene
+     * @param {String} thisGene 
+     * @returns VNode div
+     */
     const showGene = (thisGene) =>
     div('#' + absGene(thisGene) + '.col.orange.lighten-4.genePopup', geneStyle, [
         thisGene
     ])
 
-    const validVdom$ = xs.combine(validSignature$, geneAnnotationQuery.DOM)
-        .map(([s, annotation]) => div('.card .orange .lighten-3', [
-            div('.card-content .orange-text .text-darken-4', [
-                span('.card-title', 'Signature:'),
-                div('.row', { style: { fontSize: "16px", fontStyle: 'bold' } },
-                    s.split(" ").map(gene => showGene(gene))),
-                annotation
+    /**
+     * Vdom to be displayed when the signature is received and valid
+     * @const view/validVdom$
+     * @type {Stream}
+     */
+    const validVdom$ = xs.combine(validSignature$, geneAnnotationQuery.DOM, state$)
+        .map(([s, annotation, state]) => {
+            /**
+             * Signature split into an array
+             * @const view/validVdom$/arr
+             * @type {Array}
+             */
+            const arr = s.split(" ")
+            /**
+             * Show full signature or not
+             * @const view/validVdom$/showMore
+             * @type {Boolean}
+             */
+            const showMore = state.core.showMore
+            /**
+             * Signature size limit
+             * If set to 0 in the settings it means there is no limit,
+             * however if we would pass 0 to Array.splice we get nothing.
+             * Instead we have to pass Array.splice(0, undefined) which would be the same as Array.splice(0)
+             * @const view/validVdom$/showLimit
+             * @type {Number}
+             */
+            const showLimit = state.core.showLimit != 0 ? state.core.showLimit : undefined
+
+            /**
+             * Display div with a button when the signature is long, otherwise show empty div
+             * @const view/validVdom$/showLimit
+             * @type {VNode}
+             */
+            const showMoreButton = (showMore || arr.length > showLimit) ?
+                div('.row',
+                    button(".showMore .col .s4 .offset-s4 .btn .orange .darken-2", showMore ? 
+                        "Show less" : 
+                        "Show " + (arr.length - showLimit) + " more")
+                ) :
+                div()
+
+            return div('.card .orange .lighten-3', [
+                div('.card-content .orange-text .text-darken-4', [
+                    span('.card-title', 'Signature:'),
+                    div('.row', { style: { fontSize: "16px", fontStyle: 'bold' } },
+                        arr.slice(0, showLimit).map(gene => showGene(gene))),
+                    showMoreButton,
+                    annotation
+                ])
             ])
-        ]))
+        })
         .startWith(div('.card .orange .lighten-3', []))
 
     const invalidVdom$ = invalidSignature$
@@ -118,7 +192,30 @@ function view(state$, request$, response$, geneAnnotationQuery) {
     }
 }
 
-//function intent() {}
+/**
+ * SignatureGenerator intent, convert events on the dom to actions
+ * @function intent
+ * @param {Stream} domSource$ events from the dom
+ * @returns Object with:
+ *          - showMore: boolean when 'showMore' is toggled on or off
+ */
+function intent(domSource$) {
+    /**
+     * Toggles 'showMore' on and off
+     * @const intent/showMore$
+     * @type {MemoryStream}
+     */
+    const showMore$ = domSource$
+        .select(".showMore")
+        .events("click")
+        .fold((x, _) => !x , false)
+        .startWith(false)
+        .remember()
+
+    return {
+        showMore$: showMore$,
+    }
+}
 
 
 /**
@@ -178,7 +275,9 @@ function SignatureGenerator(sources) {
     const data$ = response$
         .map(r => r.body.result)
 
-    const reducers$ = model(newInput$, request$, data$)
+    const actions = intent(sources.DOM)
+
+    const reducers$ = model(newInput$, request$, data$, actions.showMore$)
 
     const views = view(state$, request$, response$, geneAnnotationQuery)
 

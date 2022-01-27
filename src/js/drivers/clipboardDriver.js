764b807a65168c353ed5ec265a5b6f04524cd673
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-01-27 10:40:44 +0100

    Add clipboard driver functionality

--- src/js/drivers/clipboardDriver.js
+++ src/js/drivers/clipboardDriver.js
@@ -1,8 +1,13 @@
 function clipboardDriver(stream$) {
   stream$.addListener({
     next: message => {
-        console.log("text that should be placed in clipboard: " + message)
-        //message.map(m => console.log(m))
+        // console.log("text that should be placed in clipboard: " + message)
+        navigator.clipboard.writeText(message).then(function() {
+            /* clipboard successfully set */
+        }, function() {
+            /* clipboard write failed */
+            console.warn("Writing to clipboard failed")
+        });
     },
     error: e => console.error(e),
     complete: () => {}

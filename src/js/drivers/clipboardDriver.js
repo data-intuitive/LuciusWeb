0efbe0e2302ee935deca5181f7ec683e50f0ee0e
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-02-02 10:06:47 +0100

    Provide way to paste images in clipboard
    copying of plots is with fixed data

--- src/js/drivers/clipboardDriver.js
+++ src/js/drivers/clipboardDriver.js
@@ -1,13 +1,24 @@
 function clipboardDriver(stream$) {
   stream$.addListener({
     next: message => {
-        // console.log("text that should be placed in clipboard: " + message)
+
+      if (typeof message === 'object') {
+        console.log('object received for clipboard, type: ' + message.type)
+        navigator.clipboard.write([
+          new ClipboardItem({
+              [message.type]: message.data
+          })
+        ]);
+      }
+      else {
+        console.log("text that should be placed in clipboard: " + message)
         navigator.clipboard.writeText(message).then(function() {
             /* clipboard successfully set */
         }, function() {
             /* clipboard write failed */
             console.warn("Writing to clipboard failed")
         });
+      }
     },
     error: e => console.error(e),
     complete: () => {}

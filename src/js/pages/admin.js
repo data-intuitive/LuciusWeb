e4924035c439f28899746372e66efbd2f3fb179f
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-03-04 12:43:20 +0100

    Add settings section and start using Intent Model View
    use api address from deployment instead of hard coded

--- src/js/pages/admin.js
+++ src/js/pages/admin.js
@@ -1,8 +1,10 @@
 import xs from "xstream"
 import { div, p, span, button, textarea, input } from "@cycle/dom"
+import isolate from "@cycle/isolate"
 import { merge, prop, equals } from "ramda"
 
 import { initSettings } from "../configuration"
+import { SettingsEditor } from "../components/SettingsEditor"
 import debounce from "xstream/extra/debounce"
 import dropRepeats from "xstream/extra/dropRepeats"
 import sampleCombine from "xstream/extra/sampleCombine"
@@ -16,13 +18,67 @@ const defaultState = {
   },
 }
 
+const settingsConfig = [
+  {
+    group: "reload",
+    title: "API Settings",
+    settings: [
+      {
+        field: "url",
+        class: ".input-field",
+        type: "text",
+        title: "Spark JobServer URL",
+        props: {},
+      },
+      {
+        field: "contextOptions",
+        class: ".input-field",
+        type: "text",
+        title: "Context options",
+        props: {},
+      },
+    ],
+  },
+]
+
+function intent(domSource$) {
+  const trigger1$ = domSource$.select(".trigger1").events("click").remember()
+  const trigger2$ = domSource$.select(".trigger2").events("click").remember()
+  const trigger3$ = domSource$.select(".trigger3").events("click").remember()
+  const trigger4$ = domSource$.select(".trigger4").events("click").remember()
+  const trigger5$ = domSource$.select(".trigger5").events("click").remember()
+
+  const loadJar$ = domSource$.select(".jarFile").events("change").remember().debug("loadJar$")
+  const loadConfig$ = domSource$.select(".configFile").events("change").remember().debug("loadConfig$")
+
+  return {
+    trigger1$: trigger1$,
+    trigger2$: trigger2$,
+    trigger3$: trigger3$,
+    trigger4$: trigger4$,
+    trigger5$: trigger5$,
+
+    loadJar$: loadJar$,
+    loadConfig$: loadConfig$,
+  }
+}
+
+function model() {
+
+}
+
+function view() {
+
+}
+
+
 /**
  * This component should become an API managemt page used to trigger initialization
  * and other operational aspects of managing LuciusAPI.
  */
 function Admin(sources) {
   const state$ = sources.onion.state$.debug("state$")
-    .compose(dropRepeats((x, y) => equals(x.core, y.core)))
+    .compose(dropRepeats(equals))
     .startWith({ core: defaultState, settings: initSettings })
   // .map(state => merge(state, state.settings.admin, state.settings.api))
 
@@ -44,6 +100,8 @@ function Admin(sources) {
     )
     .flatten()
 
+  const actions = intent(sources.DOM)
+
   /**
    * Parse the successful results only.
    *
@@ -56,23 +114,19 @@ function Admin(sources) {
     .flatten()
     .compose(debounce(500))
 
-  const trigger1$ = sources.DOM.select(".trigger1").events("click").remember()
-  const trigger2$ = sources.DOM.select(".trigger2").events("click").remember()
-  const trigger3$ = sources.DOM.select(".trigger3").events("click").remember()
-  const trigger4$ = sources.DOM.select(".trigger4").events("click").remember()
-  const trigger5$ = sources.DOM.select(".trigger5").events("click").remember()
+  const Settings = isolate(SettingsEditor, "settings")({
+    ...sources,
+    settings$: xs.of(settingsConfig)
+  })
 
-  const loadJar$ = sources.DOM.select(".jarFile").events("change").remember().debug("loadJar$")
-  const loadConfig$ = sources.DOM.select(".configFile").events("change").remember().debug("loadConfig$")
-
-  const jarFile$ = loadJar$
+  const jarFile$ = actions.loadJar$
     .map((_) => {
       const input = document.getElementById("jarFile")      
       const file = input.files[0]
       return file
     })
 
-  const configFile$ = loadConfig$
+  const configFile$ = actions.loadConfig$
     .map((_) => {
       const input = document.getElementById("configFile")      
       const file = input.files[0]
@@ -81,9 +135,9 @@ function Admin(sources) {
 
   // Deleting previous context...
   // curl -X DELETE localhost:8090/contexts/luciusapi
-  const requestTrigger1$ = trigger1$.compose(sampleCombine(state$))
+  const requestTrigger1$ = actions.trigger1$.compose(sampleCombine(state$))
     .map(([_, state]) => ({
-      url: "http://localhost:8090/contexts/luciusapi",
+      url: state.settings.reload.url + "contexts/luciusapi",
       method: "DELETE",
       send: {},
       category: "init",
@@ -91,9 +145,9 @@ function Admin(sources) {
 
   // Uploading assembly jar...
   // curl -H Content-Type: application/java-archive --data-binary @target/scala-2.11/LuciusAPI-assembly-5.0.1.jar localhost:8090/binaries/luciusapi
-  const requestTrigger2$ = trigger2$.compose(sampleCombine(xs.combine(state$, jarFile$)))
+  const requestTrigger2$ = actions.trigger2$.compose(sampleCombine(xs.combine(state$, jarFile$)))
     .map(([_, [state, jar]]) => ({
-      url: "http://localhost:8090/binaries/luciusapi",
+      url: state.settings.reload.url + "binaries/luciusapi",
       method: "POST",
       type: "application/java-archive",
       send: jar,
@@ -102,9 +156,9 @@ function Admin(sources) {
 
   // Starting new context...
   // curl -d  localhost:8090/contexts/luciusapi?context-factory=spark.jobserver.context.SessionContextFactory&spark.scheduler.mode=FAIR&spark.jobserver.context-creation-timeout=60&spark.memory.fraction=0.7&spark.dynamicAllocation.enabled=false&spark.executor.instances=6&spark.executor.cores=4&spark.executor.memory=4g&spark.yarn.executor.memoryOverhead=2g&spark.yarn.am.memory=4G&spark.driver.memory=4G
-  const requestTrigger3$ = trigger3$.compose(sampleCombine(state$))
+  const requestTrigger3$ = actions.trigger3$.compose(sampleCombine(state$))
     .map(([_, state]) => ({
-      url: "http://localhost:8090/contexts/luciusapi?context-factory=spark.jobserver.context.SessionContextFactory&spark.scheduler.mode=FAIR&spark.jobserver.context-creation-timeout=60&spark.memory.fraction=0.7&spark.dynamicAllocation.enabled=false&spark.executor.instances=6&spark.executor.cores=4&spark.executor.memory=4g&spark.yarn.executor.memoryOverhead=2g&spark.yarn.am.memory=4G&spark.driver.memory=4G",
+      url: state.settings.reload.url + "contexts/luciusapi?" + state.settings.reload.contextOptions,
       method: "POST",
       send: {},
       category: "init",
@@ -112,17 +166,17 @@ function Admin(sources) {
 
   // Initializing API...
   // curl --data-binary @utils/../../config/spark_config.conf localhost:8090/jobs?context=luciusapi&appName=luciusapi&classPath=com.dataintuitive.luciusapi.initialize
-  const requestTrigger4$ = trigger4$.compose(sampleCombine(xs.combine(state$, configFile$)))
+  const requestTrigger4$ = actions.trigger4$.compose(sampleCombine(xs.combine(state$, configFile$)))
     .map(([_, [state, config]]) => ({
-      url: "http://localhost:8090/jobs?context=luciusapi&appName=luciusapi&classPath=com.dataintuitive.luciusapi.initialize",
+      url: state.settings.reload.url + "jobs?context=luciusapi&appName=luciusapi&classPath=com.dataintuitive.luciusapi.initialize",
       method: "POST",
       send: config,
       category: "init",
     }))
 
-  const requestTrigger5$ = trigger5$.compose(sampleCombine(state$))
+  const requestTrigger5$ = actions.trigger5$.compose(sampleCombine(state$))
     .map(([_, state]) => ({
-      url: "http://localhost:8090/jobs?status=finished",
+      url: state.settings.reload.url + "jobs?status=finished",
       method: "DELETE",
       send: {},
       category: "init",
@@ -158,10 +212,11 @@ function Admin(sources) {
     )
     .fold((acc, t) => acc + t + "\r\n", "")
 
-  const vdom$ = xs.combine(state$, initText$)
-    .map(([state, initText]) =>
+  const vdom$ = xs.combine(state$, Settings.DOM, initText$)
+    .map(([state, settings, initText]) =>
       div(".container", [
         div([p("Server poll status: " + (state.core?.state == undefined ? "no reply received" : "reply successfully received"))]),
+        settings,
         div(".row .s12"),
         div(".row .s12", [
           span(".col .s1", "Step 1"),
@@ -208,10 +263,12 @@ function Admin(sources) {
     core: { ...prevState.core, state: 1 },
   }))
 
+  const settingsReducer$ = Settings.onion.compose(debounce(200))
+
   return {
     DOM: vdom$,
     HTTP: xs.merge(request$, requestTrigger1$, requestTrigger2$, requestTrigger3$, requestTrigger4$, requestTrigger5$),
-    onion: xs.merge(defaultReducer$, responseReducer$),
+    onion: xs.merge(defaultReducer$, responseReducer$, settingsReducer$),
   }
 }
 

6b9688bac13890f9ff0d32b861b44a0885703cf1
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-11-17 11:38:24 +0100

    Use props to retrieve the api url

--- src/js/components/InformationDetails.js
+++ src/js/components/InformationDetails.js
@@ -4,14 +4,14 @@ import dropRepeats from 'xstream/extra/dropRepeats'
 export function InformationDetails(sources) {
 
   const state$ = sources.onion.state$
+  const props$ = sources.props
 
   // Combine with deployments to the up-to-date endpoint config
   const triggerQuery$ = state$.compose(dropRepeats())
 
-  const request$ = triggerQuery$
-    .map(state => ({
-      // url: state.settings.api.url + '&classPath=com.dataintuitive.luciusapi.perturbationInformationDetails',
-      url: 'http://localhost:8090/jobs?context=luciusapi&appName=luciusapi&sync=true&timeout=30' + '&classPath=com.dataintuitive.luciusapi.perturbationInformationDetails',
+  const request$ = xs.combine(triggerQuery$, props$)
+    .map(([state, props]) => ({
+      url: props.api.url + '&classPath=com.dataintuitive.luciusapi.perturbationInformationDetails',
       method: 'POST',
       send: {
         'query': state.id
@@ -26,7 +26,7 @@ export function InformationDetails(sources) {
   // TODO: fall back to default filters set in config file
   const validResponse$ = response$$
     .map(response$ =>
-      response$.replaceError(_ => xs.of({processing_level: "error"}))
+      response$.replaceError(_ => xs.of({}))
     )
     .flatten()
 
